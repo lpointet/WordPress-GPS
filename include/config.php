24e1ed4bdeb52eafc36f1c20cfb0d741de08bfd9
Lionel POINTET <lionel.pointet@globalis-ms.com>
2012-07-05 16:13:49 +0200

    Bugfix - Fixed textdomain name & set English as default language

--- include/config.php
+++ include/config.php
@@ -7,4 +7,4 @@ define('GB_GPS_COMPLETE_PATH', WP_PLUGIN_DIR.'/'.GB_GPS_PATH);
 define('GB_GPS_URL', WP_PLUGIN_URL.'/'.GB_GPS_PATH);
 
 // Name of the text domain
-define('GB_GPS_TEXT_DOMAIN', 'gp_gps');
+define('GB_GPS_TEXT_DOMAIN', 'gb_gps');
